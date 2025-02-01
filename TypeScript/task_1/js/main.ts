2a9db307c16621057500e2a05ac2ff1d26c9365e
juli <julietabobadilla123@gmail.com>
2025-02-01 18:06:48 -0300

    Task 4 - Function  interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -20,3 +20,13 @@ const director1: Directors = {
     numberOfReports: 17,
   };
   console.log(director1);
+
+interface printTeacherFunction {
+    (firstName: string, lastName: string): string;
+}
+
+const printTeacher: printTeacherFunction = (firstName, lastName) => {
+    return `${firstName.charAt(0)}. ${lastName}`;
+};
+
+console.log(printTeacher("John", "Doe"));
