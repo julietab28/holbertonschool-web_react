ad4363306419ee714e6fdc60c7b4591246099873
juli <julietabobadilla123@gmail.com>
2025-02-01 18:05:08 -0300

    Task 3 - Director interface

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -6,3 +6,17 @@ interface Teacher {
     location: string;
     [anything: string]: any;
 }
+
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    yearsOfExperience: 10,
+    numberOfReports: 17,
+  };
+  console.log(director1);
